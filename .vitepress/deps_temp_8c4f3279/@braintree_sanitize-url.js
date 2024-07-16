9f146be369d0e1c6d281544455e5d8b90c58062c
zhouyuanxinand <139625638+zhouyuanxinand@users.noreply.github.com>
2024-07-17 00:40:09 +0800

    Add files via upload

---
+++ .vitepress/deps_temp_8c4f3279/@braintree_sanitize-url.js
@@ -0,0 +1,67 @@
+import {
+  __commonJS
+} from "./chunk-Y2F7D3TJ.js";
+
+// node_modules/.pnpm/@braintree+sanitize-url@7.0.1/node_modules/@braintree/sanitize-url/dist/constants.js
+var require_constants = __commonJS({
+  "node_modules/.pnpm/@braintree+sanitize-url@7.0.1/node_modules/@braintree/sanitize-url/dist/constants.js"(exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.BLANK_URL = exports.relativeFirstCharacters = exports.urlSchemeRegex = exports.ctrlCharactersRegex = exports.htmlCtrlEntityRegex = exports.htmlEntitiesRegex = exports.invalidProtocolRegex = void 0;
+    exports.invalidProtocolRegex = /^([^\w]*)(javascript|data|vbscript)/im;
+    exports.htmlEntitiesRegex = /&#(\w+)(^\w|;)?/g;
+    exports.htmlCtrlEntityRegex = /&(newline|tab);/gi;
+    exports.ctrlCharactersRegex = /[\u0000-\u001F\u007F-\u009F\u2000-\u200D\uFEFF]/gim;
+    exports.urlSchemeRegex = /^.+(:|&colon;)/gim;
+    exports.relativeFirstCharacters = [".", "/"];
+    exports.BLANK_URL = "about:blank";
+  }
+});
+
+// node_modules/.pnpm/@braintree+sanitize-url@7.0.1/node_modules/@braintree/sanitize-url/dist/index.js
+var require_dist = __commonJS({
+  "node_modules/.pnpm/@braintree+sanitize-url@7.0.1/node_modules/@braintree/sanitize-url/dist/index.js"(exports) {
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.sanitizeUrl = void 0;
+    var constants_1 = require_constants();
+    function isRelativeUrlWithoutProtocol(url) {
+      return constants_1.relativeFirstCharacters.indexOf(url[0]) > -1;
+    }
+    function decodeHtmlCharacters(str) {
+      var removedNullByte = str.replace(constants_1.ctrlCharactersRegex, "");
+      return removedNullByte.replace(constants_1.htmlEntitiesRegex, function(match, dec) {
+        return String.fromCharCode(dec);
+      });
+    }
+    function sanitizeUrl(url) {
+      if (!url) {
+        return constants_1.BLANK_URL;
+      }
+      var charsToDecode;
+      var decodedUrl = url;
+      do {
+        decodedUrl = decodeHtmlCharacters(decodedUrl).replace(constants_1.htmlCtrlEntityRegex, "").replace(constants_1.ctrlCharactersRegex, "").trim();
+        charsToDecode = decodedUrl.match(constants_1.ctrlCharactersRegex) || decodedUrl.match(constants_1.htmlEntitiesRegex) || decodedUrl.match(constants_1.htmlCtrlEntityRegex);
+      } while (charsToDecode && charsToDecode.length > 0);
+      var sanitizedUrl = decodedUrl;
+      if (!sanitizedUrl) {
+        return constants_1.BLANK_URL;
+      }
+      if (isRelativeUrlWithoutProtocol(sanitizedUrl)) {
+        return sanitizedUrl;
+      }
+      var urlSchemeParseResults = sanitizedUrl.match(constants_1.urlSchemeRegex);
+      if (!urlSchemeParseResults) {
+        return sanitizedUrl;
+      }
+      var urlScheme = urlSchemeParseResults[0];
+      if (constants_1.invalidProtocolRegex.test(urlScheme)) {
+        return constants_1.BLANK_URL;
+      }
+      return sanitizedUrl;
+    }
+    exports.sanitizeUrl = sanitizeUrl;
+  }
+});
+export default require_dist();
+//# sourceMappingURL=@braintree_sanitize-url.js.map
